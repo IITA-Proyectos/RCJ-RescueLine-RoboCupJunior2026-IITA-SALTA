222a7c1c0a8c32a9efd9eed3fb2a6a7e83db158c
benjaminvillagran <villagranbenjamin52@gmail.com>
2024-05-11 18:07:23 -0300

    agrego switch sistema de encendido

--- test/sistema_encendido/switch.cpp
+++ test/sistema_encendido/switch.cpp
@@ -3,13 +3,11 @@
 void setup()
 {
   // put your setup code here, to run once:
-  pinMode(16, OUTPUT);
-  pinMode(17, INPUT_PULLUP);
+  pinMode(32, INPUT_PULLUP);
   pinMode(13, OUTPUT);
-  digitalWrite(16, LOW);
 }
 
 void loop()
 {
-  digitalWrite(13, digitalRead(17));
+  digitalWrite(13, digitalRead(32));
 }
