28ecf970dedbd4e09bf4778121b0505826505a4d
Laureano Monteros <139661320+Laumonteros@users.noreply.github.com>
2024-07-20 19:13:33 -0300

    correcion de un par de pines

--- test/actuators/drivebaseLibTest.cpp
+++ test/actuators/drivebaseLibTest.cpp
@@ -5,25 +5,25 @@
 // https://www.arduino.cc/reference/en/
 // https://www.pjrc.com/teensy/td_timing_IntervalTimer.html
 // https://www.arduino.cc/en/Hacking/libraryTutorial
-Moto bl(4, 3, 2);
-Moto fl(35, 34, 36); // pwm dir enc
-Moto br(7, 6, 5);
-Moto fr(30, 28, 29);
-DriveBase robot(&fl, &fr, &bl, &br);
+Moto bl(29, 28, 27);
+Moto fl(7, 6, 5); // pwm dir enc
+Moto br(36, 37, 38);
+Moto fr(4, 3, 2);
 
-void ISR1() { bl.updatePulse(); }
+ 
 void ISR2() { fl.updatePulse(); }
 void ISR3() { br.updatePulse(); }
 void ISR4() { fr.updatePulse(); }
+DriveBase robot(&fl, &fr, &bl, &br);
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(2), ISR1, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(36), ISR2, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(5), ISR3, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(29), ISR4, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(27), ISR1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(5), ISR2, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(38), ISR3, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(2), ISR4, CHANGE);
 }
 
 int counter = 0;
@@ -31,4 +31,7 @@ void loop()
 {
   // put your main code here, to run repeatedly:
   robot.steer(20, LOW, 0);
+  delay(2000);
+  robot.sterr(20, HIGH, 0);
+  delay(2000);  
 }
