da84f55ea340cbb399ae5db3fc565a8514d6781f
Benjamin Villagran <148703811+benjaminvillagran@users.noreply.github.com>
2025-03-22 19:14:22 -0300

    final_main_silver

--- rpi/final_rpi/main_silver.py
+++ rpi/final_rpi/main_silver.py
@@ -5,23 +5,23 @@ import math
 import time
 import serial
 
-debugOriginal = False
-debugBlack = False
-debugHori = False
-debugGreen = False
-debugBlue = False
+debugOriginal = True
+debugBlack =True
+debugGreen = True
+debugBlue = True
+debugHori=True
 record = False
 noise_blob_threshold = 16
+min_square_size = 150
 
 vs = WebcamVideoStream(src=0).start()
 ser = serial.Serial('/dev/serial0', 115200)
-
-lower_black = np.array([0, 0, 0])  # BGR
+lower_black = np.array([0, 0,0])  # BGR
 upper_black = np.array([70, 70, 70])
 lower_green = np.array([0, 100, 80])  # lab
 upper_green = np.array([255, 120, 150])
-lower_silver_hsv = np.array([20,2,90])  # Valores en espacio HSV para detectar el plateado
-upper_silver_hsv = np.array([35, 20, 175])  # Ajusta estos valores si es necesario
+lower_silver_hsv = np.array([20,20,75])  # Valores en espacio HSV para detectar el plateado
+upper_silver_hsv = np.array([40, 60, 190])  # Ajusta estos valores si es necesario
 
 test_frame = vs.read()
 width, height = test_frame.shape[1], test_frame.shape[0]
@@ -32,7 +32,7 @@ cam_y = height - 1      # 119 ~ bottom row
 
 timer_active = False
 green_output_duration = 1  # Duración en segundos para mostrar el estado
-green_output_cooldown_duration = 2.5  # Duración en segundos para estar en 0
+green_output_cooldown_duration = 2  # Duración en segundos para estar en 0
 green_state_final = 0
 timer_active = False
 timer_start_time = 0
@@ -47,7 +47,6 @@ for i in range(height):
         x_com[i][j] = (j - cam_x) / (width / 2)   # [-1, 1]
         y_com[i][j] = (cam_y - i) / height        # [0, 1]
 
-cccounter = 0
 
 while True:
     frame = vs.read()
@@ -61,16 +60,7 @@ while True:
     x_black *= (1 - y_com)
     y_black = cv2.bitwise_and(y_com, y_com, mask=black_mask)
 
-    # FILTER GREEN PIXELS
-    green_mask = cv2.inRange(lab, lower_green, upper_green)
-    green_mask[:90, :] = 0
-
-    # DETECT GREEN SQUARES
-    green_contours, hierarchy = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    green_contours = [c for c in green_contours if cv2.contourArea(c) > noise_blob_threshold]
-    green_mask = cv2.drawContours(green_mask, green_contours, -1, 255, -1)
-
-    x_green = cv2.bitwise_and(x_com, x_com, mask=green_mask)
+    green_mask = cv2.inRange(lab[90:, :, :], lower_green, upper_green)    # shape = (30, 160)
 
     # CALCULATE RESULTANT
     green_state = 0
@@ -79,21 +69,45 @@ while True:
     angle = (math.atan2(y_resultant, x_resultant) / math.pi * 180) - 90
     speed = 25
 
-    if len(green_contours) == 1:
-        if np.mean(x_green) < 0:  # turn left
-            green_state = 1
-            speed = 30
-            angle = 45  
+    if np.sum(green_mask) > min_square_size * 255:
+
+        green_pixels = np.amax(green_mask, axis = 0)    # for every column, if green: 1 else 0, shape = (160,)
+           
+        greenIndices = np.where(green_pixels == np.max(green_pixels))   # get indices of columns that have green
+        leftIndex = greenIndices[0][0]
+        rightIndex = greenIndices[0][-1]
+        slicedGreen = frame[60:90, leftIndex:rightIndex + 1, :]
+
+        greenCentroidX = (rightIndex + leftIndex) / 2
+        slicedBlackMaskAboveGreen = black_mask[60:90, leftIndex:rightIndex + 1]
+        blackM = cv2.moments(black_mask[90:, :])
+
+        if np.sum(black_mask[90:, :]):  # if there is black, prevents divide by 0 error
+            cx_black = int(blackM["m10"] / blackM["m00"])   # get x-value of black centroid
+
+        if (np.sum(slicedBlackMaskAboveGreen) / (255 * 30 * (rightIndex - leftIndex))) > 0.32:   # if mean is high -> square before line
+            greenSquare = False
+            filtered_green_mask = cv2.erode(green_mask, kernel)
+            filtered_green_mask = cv2.dilate(green_mask, kernel)
+            green_contours, hierarchy = cv2.findContours(filtered_green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            if len(green_contours) > 1 and cx_black > leftIndex and cx_black < rightIndex and np.sum(green_mask) > (2 * min_square_size * 255):
+                green_state = 3
+                
+            elif greenCentroidX < cx_black:
+
+                green_state = 1
+
+            else:
+
+                green_state = 2
+
         else:
-            green_state = 2  # turn right
-            speed = 30
-            angle = -45
+            greenSquare = False
+            green_state = 0
 
-    if len(green_contours) == 2:
-        green_state = 3
-
-    if y_resultant == 0:
-        angle = 0
+    else:
+        greenSquare = False
+        green_state = 0
 
     # DETECTION OF SILVER LINE (IN HSV)
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # Convertir la imagen de BGR a HSV
@@ -104,52 +118,14 @@ while True:
     silver_line = False
     for contour in silver_contours:
         area = cv2.contourArea(contour)
-        if area > 2:  # Define un umbral para el área para evitar ruido
+        if area > 800:  # Define un umbral para el área para evitar ruido
             silver_line = True
             break
 
-    # CONTINUE WITH GREEN STATE CALCULATION
-    contornos_izquierda = 0
-    contornos_derecha = 0
-    for contorno in green_contours:
-        x, y, w, h = cv2.boundingRect(contorno)
-        if x + w / 2 < cam_x:
-            contornos_izquierda += 1
-        else:
-            contornos_derecha += 1
-
-    if contornos_izquierda > 0 and contornos_derecha > 0:
-        green_state_contorno = 3
-    elif contornos_izquierda > 0:
-        green_state_contorno = 1
-    elif contornos_derecha > 0:
-        green_state_contorno = 2
-    else:
-        green_state_contorno = 0
-
-    green_state_final = green_state
-
-    if green_state_final != 0:
-        if not timer_active:
-            green_output = green_state_final
-            timer_active = True
-            timer_start_time = time.time()
-        else:
-            elapsed_time = time.time() - timer_start_time
-            if elapsed_time < green_output_duration:
-                green_output = green_state_final
-            elif elapsed_time < (green_output_duration + green_output_cooldown_duration):
-                green_output = 0
-            else:
-                timer_active = False
-                green_output = green_state_final
-    else:
-        green_output = green_state_final
-    
     # SEND OUTPUT WITH SILVER LINE DETECTION
     output = [255, speed,
               254, round(angle) + 90,
-              253, green_output,
+              253, green_state,
               252, int(silver_line)]  # 1 si se detecta la línea plateada, 0 en caso contrario
 
     print(output)
